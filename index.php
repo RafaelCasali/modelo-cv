f1761d3547e753927a0a621df87ec1a6b97831e4
rpcasali <rafael@rafaelcasali.com>
2017-03-21 15:04:03 -0300

    CV

---
+++ index.php
@@ -0,0 +1,153 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+
+    <meta name="keywords" content="" http-equiv="keywords" /> 
+    <meta name="description" content="" />
+    <meta name="robots" content="all" /> 
+    <meta name="language" content="pt-br" /> 
+    <meta name="classification" content="" /> 
+    <meta name="rating" content="general" /> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">    
+
+	<meta property="og:title" content="Rafael Casali" />
+	<meta property="og:url" content="http://www.rafaelcasali.com" />
+	<meta property="og:image" content="http://www.rafaelcasali.com/cv/images/foto.png" />
+	<meta property="og:site_name" content="rafaelcasali.com" />
+	<meta property="og:description" content="" />
+
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,500,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+
+    <script type='text/javascript' src='http://www.agenciataio.com.br/js/jquery-1.7.1.min.js'></script>
+    <script type='text/javascript' src='http://www.agenciataio.com.br/js/jquery.easing.1.3.js'></script> 
+
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<link rel="shortcut icon" href="favicon.ico" />
+
+    <script type="text/javascript">
+        $(function(){   
+                var nav = $('#topo');   
+                $(window).scroll(function () { 
+                    if ($(this).scrollTop() > 50) { 
+                        nav.addClass("topo-fixo"); 
+                    } else { 
+                        nav.removeClass("topo-fixo"); 
+                    } 
+                });  
+            });
+
+        $(function(){   
+                var nav = $('#arrow');   
+                $(window).scroll(function () { 
+                    if ($(this).scrollTop() > 50) { 
+                        nav.addClass("arrow-fixo"); 
+                    } else { 
+                        nav.removeClass("arrow-fixo"); 
+                    } 
+                });  
+            });
+    </script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-20354974-4', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Rafael Casali - CV</title>
+</head>
+
+<body>
+
+<div id="topo" class="topo">
+    <img src="images/foto.png" />
+    <p><strong>Rafael Piccini Casali</strong><br/>Desenvolvedor Front-end</p>    
+</div>
+
+<a href="#perfil"><i class="zmdi zmdi-chevron-up zmdi-hc-2x arrow" id="arrow"></i></a>
+
+<div class="perfil" id="perfil">
+        <img src="images/foto.png" class="foto" />
+        <h1 class="nome">Rafael Piccini Casali</h1>
+        <p class="cargo">Desenvolvedor Front-end</p>
+        <p class="cidade">São José dos Campos/SP</p>
+        <p class="contato"><a href="callto:12991258812">(12) 99125.8812</a> | <a href="mailto:rafael@rafaelcasali.com">rafael@rafaelcasali.com</a></p>
+
+        <div class="midias">
+            <div class="icone"><a href="https://br.linkedin.com/in/rafael-piccini-casali-08739761"><i class="zmdi zmdi-linkedin zmdi-hc-lg"></i></div> 
+            <div class="icone"><a href="callto:faelook.pc"><i class="zmdi zmdi-skype zmdi-hc-lg"></i></div>
+            <div class="icone"><a href="mailto:012991258812"><i class="zmdi zmdi-email zmdi-hc-lg"></i></a></div>
+        </div>
+</div>
+
+<div class="descricao">
+    <div class="item">
+        <h2>RESUMO</h2>
+        <p>Profissional com mais de 6 anos de experiência na área, atuando na área de desenvolvimento front-end desde 2010. Atualmente trabalha como desenvolvedor front-end na Agência Taió, em São José dos Campos/SP. Possui sólidos conhecimentos em HTML5, CSS3, JavaScript, PHP, API Google Maps, PHPMailer, Wordpress, Google Analytics, SEO, usabilidade e design responsivo.</p>
+    </div>
+    <div class="item">
+        <h2>CONHECIMENTOS</h2>
+        <p>HTML5, CSS3, JavaScript, PHP, API Google Maps, PHPMailer, Google Analytics, SEO, usabilidade, design responsivo.</p>
+    </div>
+    <div class="item">
+        <h2>EXPERIÊNCIA</h2>
+        <p>Desenvolvedor Front-end</p>
+        <p  class="barra">Agência Taió<br/>
+        (out/2010 - atualmente)<br/></p>
+    </div>
+    <div class="item">
+        <h2>FORMAÇÃO ACADÊMICA</h2>
+        <p>Análise e Desenvolvimento de Sistemas</p>
+        <p class="barra">
+        Universidade Paulista<br/>
+        (2017 - 2018)</p>
+    </div>      
+    <div class="item">
+        <h2>PROJETOS</h2>
+
+        <h3><a href="http://www.milenacavichi.com.br" target="_blank">Milena Cavichi</a></h3>
+        <p class="barra">Desenvolvimento do front e back-end. Responsivo.<br/>
+        Blog desenvolvido em Wordpress com template feito do zero.</p>
+        <p class="data barra">Desenvolvido em 2017</p>
+
+        <h3><a href="http://www.motofacilsjc.com.br" target="_blank">Moto Facil SJC</a></h3>
+        <p class="barra">Desenvolvimento do front e back-end tanto do site quanto do admin. Responsivo.
+        Sistema de chat via e-mail, vinculado com o produto escolhido no site.</p>        
+        <p class="data barra">Desenvolvido em 2017</p>
+
+        <h3><a href="http://www.agenciataio.com.br" target="_blank">Agência TAIÓ</a></h3>
+        <p class="barra">Desenvolvimento do front-end. Responsivo.</p>        
+        <p class="data barra">Desenvolvido em 2016</p>
+
+        <h3><a href="http://www.naturalmotion.com.br" target="_blank">Natural Motion</a></h3>
+        <p class="barra">Desenvolvimento do front e back-end, administração do site e sistema para consultores efetuarem pedidos com base nos produtos cadastrados. Responsivo.</p>
+        <p class="data barra">Desenvolvido em 2016</p>
+
+        <h3><a href="http://www.dumaspersonal.com.br/" target="_blank">Dumas</a></h3>
+        <p class="barra">Desenvolvimento do front-end. Responsivo.</p>
+        <p class="data barra">Desenvolvido em 2016</p>
+    
+        <h3><a href="http://www.mult-e.com.br/" target="_blank">Mult-e</a></h3>
+        <p class="barra">Desenvolvimento do front-end, área de notícias desenvolvida em Wordpress com template feito do zero.</p>
+        <p class="data barra">Desenvolvido em 2014</p>
+    </div>
+    <div class="item">
+        <h2>IDIOMA E FERRAMENTAS</h2>
+        <ul>
+            <li>Inglês (intermediário)</li>
+            <li>Adobe Photoshop</li>
+            <li>Adobe Illustrator</li>
+            <li>FileZilla</li>
+            <li>Sublime Text</li>
+        </ul>
+    </div>   
+</div>
+</body>
+</html>
